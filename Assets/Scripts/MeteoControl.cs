aaa1e83945af4a1be30f9e19f5f137fbcfe4e5dd
Hideya Kato <hideyakato@hideyakato.local>
2017-02-25 13:11:09 +0900

    add scripts for spaceship and meteos

---
+++ Assets/Scripts/MeteoControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoControl : MonoBehaviour {
+
+	//スピード
+	float Z_speed = 0.7f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//回転させる
+		transform.Rotate (new Vector3 (0, 0, transform.localRotation.z + Random.Range (0, 1.0f)));
+
+		//逆向き（spaceshipに向かって）に移動させる
+		transform.Translate (0, 0, -1 * Z_speed);
+
+		//カメラの近くまで来たら消える
+		if (transform.localPosition.z < -15) {
+			Destroy (this.gameObject);
+		}
+	}
+}
